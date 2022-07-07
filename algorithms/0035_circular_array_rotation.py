75b6430975056938cf7e7fda82799b53dd08065c
garrou <39885692+garrou@users.noreply.github.com>
2022-07-07 10:56:44 +0200

    Add 35

--- algorithms/0035_circular_array_rotation.py
+++ algorithms/0035_circular_array_rotation.py
@@ -1,2 +1,25 @@
 # https://www.hackerrank.com/challenges/circular-array-rotation/problem
 
+def circularArrayRotation(a: list[int], k: int, queries: list[int]) -> list[int]:
+    l = len(a)
+    res = [0] * l
+        
+    for i, elt in enumerate(a):
+        res[(i + k) % l] = elt
+
+    return [res[i] for i in queries]
+
+"""
+rotate array
+
+tmp1 = a[0]
+tmp2 = a[1]
+
+for _ in range(k):
+        for i in range(1, l):
+            right = (i+1) % l
+            a[i] = tmp1
+            tmp1 = tmp2
+            tmp2 = a[right]
+            a[right] = tmp1
+"""
