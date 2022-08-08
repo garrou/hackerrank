91a735e739e0826513401bce21e4d52be42642ed
Adrien <39885692+1-irdA@users.noreply.github.com>
2022-08-09 01:59:07 +0200

    Remove space

--- python/medium/company_logo.py
+++ python/medium/company_logo.py
@@ -9,7 +9,7 @@ def most_commons(s: str) -> None:
         d[letter] = d.get(letter, 0) + 1
 
     # sort by key and then by val
-    d = {val[0] : val[1] for val in sorted(d.items(), key = lambda x: (-x[1], x[0]))[:LIMIT]}
+    d = {val[0] : val[1] for val in sorted(d.items(), key=lambda x: (-x[1], x[0]))[:LIMIT]}
 
     for k, v in d.items():
         print(f'{k} {v}')
